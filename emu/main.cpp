50ba5e849135d3eff951d5415df4637d5b2b90b3
bwburnsides <19192452+bwburnsides@users.noreply.github.com>
2023-06-25 17:24:07 -0500

    updated argument parsing

--- emu/main.cpp
+++ emu/main.cpp
@@ -7,86 +7,94 @@
 #include "dis.h"
 #include "bus.h"
 
-int parse_arguments(
-    int argc,
-    char* argv[],
-    const char** rom_name,
-    int* max_inst_cycles,
-    bool* show_disassembly
-) {
-    *show_disassembly = false;
-    *max_inst_cycles = 100;
+typedef struct {
+    char* rom_name;
+    int max_cycles;
+} ParsedArguments;
+
+void usage(const char* const program_name) {
+    fprintf(
+        stderr,
+        "usage: %s rom_file [ncycles]\n"
+        "    rom_file: input rom to initialize bus with\n"
+        "    ncycles: maximum number of instruction cycles to execute\n"
+        "             (default is 100)\n",
+        program_name
+    );
+}
+
+ParsedArguments parse_arguments(const int argc, char* const argv[]) {
+    ParsedArguments args;
+    char* const program_name = argv[0];
 
     if (argc < 2) {
-        return -1;
+        fprintf(stderr,"Provide path to ROM file.\nSee --help for usage details.\n");
+        exit(EXIT_FAILURE);
     }
 
-    *rom_name = argv[1];
+    if (strcmp(argv[1], "--help") == 0) {
+        usage(argv[0]);
+        exit(EXIT_SUCCESS);
+    }
 
-    if (argc > 2) {
-        *max_inst_cycles = atoi(argv[2]);
+    args.rom_name = argv[1];
+    args.max_cycles = 1000;
+
+    if (argc == 2) {
+        return args;
+    }
+
+    args.max_cycles = atoi(argv[2]);
+    if (!args.max_cycles && strcmp(argv[2], "0")) {
+        fprintf(
+            stderr,
+            "Optional argument ncycles must be integer (got \"%s\").\n"
+            "See --help for usage details.\n",
+            argv[2]
+        );
+        exit(EXIT_FAILURE);
     }
 
     if (argc > 3) {
-        if (strcmp(argv[3], "dis") == 0) {
-            *show_disassembly = true;
-        }
+        fprintf(stderr, "Arguments beginning with \"%s\" are invalid.\nSee --help for usage details.\n", argv[3]);
+        exit(EXIT_FAILURE);
     }
 
-    return 0;
+    return args;
 }
 
 int main(int argc, char *argv[]) {
-    const char* rom_name;
-    int max_inst_cycles = 100;
-    bool show_disassembly = false;
+    const ParsedArguments args = parse_arguments(argc, argv);
+    printf(
+        "Parsed Arguments:\n"
+        "ROM Name: %s\n"
+        "Max Cycles: %d\n\n",
+        args.rom_name,
+        args.max_cycles
+    );
 
-    if (
-        parse_arguments(
-            argc,
-            argv,
-            &rom_name,
-            &max_inst_cycles,
-            &show_disassembly
-        ) < 0
-    ) {
-        printf("Provide path to ROM file.\n");
-        return -1;
-    }
-
-    printf("Parsed arguments:\n");
-    printf("- ROM Name: %s\n", rom_name);
-    printf("- Max Instruction Cycles: %d\n", max_inst_cycles);
-    printf("- Show Disassembly: %s\n\n", show_disassembly ? "true" : "false");
-
-    bus::BusError error = bus::init(rom_name);
+    bus::BusError error = bus::init(args.rom_name);
     if (error != bus::NO_ERROR) {
-        printf(
-            "Bus initialization failed with code %s\n",
-            bus::error_repr(error)
-        );
+        printf("Bus initialization failed with code %s\n", bus::error_repr(error));
         return -1;
     }
 
-    if (show_disassembly) {
-        printf("Disassembly of ROM (%d bytes)\n", bus::ROM_SIZE);
-        bus::disassemble(stdout, bus::ROM_BASE, bus::ROM_BASE + bus::ROM_SIZE);
-        printf("\n");
-    }
+    printf("Disassembly of ROM (%d bytes)\n", bus::ROM_SIZE);
+    bus::disassemble(stdout, bus::ROM_BASE, bus::ROM_BASE + bus::ROM_SIZE);
+    printf("\n");
 
     mc8::mc8 *cpu = mc8::create(&bus::read, &bus::write);
     if (cpu == NULL) {
         printf("Failed to allocate memory for CPU.\n");
-        return -1;
+        return EXIT_FAILURE;
     }
 
     printf("%s", "Emulation beginning.\n\n");
+    uint32_t actual_cycles = mc8::run(cpu, args.max_cycles);
 
-    uint32_t actual_cycles = mc8::run(cpu, max_inst_cycles);
     printf("\nEmulation completed.\n");
     printf("Executed %d instructions.\n", actual_cycles);
 
     mc8::release(cpu);
-
-    return 0;
+    return EXIT_SUCCESS;
 }
